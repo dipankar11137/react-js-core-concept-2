b37551edc4b00be8a68149898ee75baf0054add0
Dipankar <dipankar15-11137@diu.edu.bd>
2022-03-21 01:46:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const products = [
+    { name: 'Laptop', Price: 45000 },
+    { name: 'Phone', Price: 75000 },
+    { name: 'Watch', Price: 3000 },
+    { name: 'Tablet', Price: 25000 },
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        products.map(product => <Product name={product.name} price={product.Price}></Product>)
+      }
+
+
+      {/* <Product name="Laptop" price="60000"></Product>
+      <Product name="Phone" price="80000"></Product>
+      <Product name="Watch" price="3500"></Product> */}
     </div>
   );
 }
 
+function Product(props) {
+  return (
+    <div className="product">
+      <h2>Name : {props.name}</h2>
+      <h5>Price : {props.price}</h5>
+    </div>
+  )
+}
 export default App;
+
+
+
+
+// upor ar mal clean ar janno aii jaiga raklam
