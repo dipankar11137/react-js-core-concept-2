823bbafe9cf36433cb6c7f288b5ec0f7ae09aede
Dipankar <dipankar15-11137@diu.edu.bd>
2022-03-21 10:02:19 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
 
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUser></ExternalUser>
     </div>
   );
 }
 
+function ExternalUser() {
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }, [])
+  return (
+    <div>
+      <h2>External User</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{ border: '2px solid red', padding: '20px' }}>
+      <h2>Name : {props.name}</h2>
+      <p>Email : {props.email}</p>
+    </div>
+  )
+}
 function Counter() {
   const [count, setCount] = useState(16);
 
